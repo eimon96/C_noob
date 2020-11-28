b6992caf9e5c21853a7c5d15e5d127830c50f289
Penny Simitsi <18515231+eimon96@users.noreply.github.com>
2020-11-28 23:22:23 +0200

    Add files via upload

---
+++ e96.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int main()
+{
+	system("Color 0A");
+	
+	printf("				 \n\
+							 \n\
+			   ___      __   \n\
+			  / _ \\    / /   \n\
+			  \\_, /   / _ \\  \n\
+			  _/_/_   \\___/  \n\
+			_|\"\"\"\"\"|_|\"\"\"\"\"| \n\
+			\"`-0-0-'\"`-0-0-' \n\
+							 \n\
+			\n\n\n");
+	
+	// https://patorjk.com/software/taag/#p=display&f=Train&t=96
+	
+	
+	system("pause");
+	return 0;
+}
